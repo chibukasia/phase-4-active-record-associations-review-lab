71a32a6673e8eb42e06844024ea5bb5aa32d9086
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-09-29 23:08:33 +0300

    commit tree b12acc752860b962d7acf76101c32441ca214d32

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.references :passenger, null: false, foreign_ke: true
+      t.references :taxi, null: false, foreign_ke: true
       t.timestamps
     end
   end
